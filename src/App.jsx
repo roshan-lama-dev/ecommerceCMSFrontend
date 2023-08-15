126bec6101b6b9da3c6ebb9ea16029d896c8cecf
Havochalo <officialmail85@gmail.com>
2023-08-15 21:11:09 +1000

    ecommerceFrontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MainNav } from "./components/MainNav";
+
+function App() {
+  return (
+    <>
+      <MainNav />
+    </>
+  );
+}
+
+export default App;
